6e4067fb573557f2ff3102d35f1c1a33eabb7c84
kashyaptan <kashyaptan2004@gmail.com>
2024-11-13 16:17:32 -0500

    created skeleton

---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import '../projectCss/Home.css'
+
+function Home() {
+  return (
+    <>
+        <div className="page-1">
+            {/* 
+            THIS IS WHERE THE BACKGROUND INAGES GOES
+            <img src="" alt="" /> 
+            */}
+            <div className="navbar">
+
+            </div>
+            <div className='company-name-text'>
+                FM Technology
+            </div>
+        </div>
+        <div className="page-2">
+            <div className="wallet">
+                wallet
+            </div>
+            <div className="phone-case">
+                phone case
+            </div>
+            <div className="key-chain">
+                key chain
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Home
